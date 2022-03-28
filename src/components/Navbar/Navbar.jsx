901708bcc15495ced9d3ac8464a64a65b2c47fe8
koagie <kogichan.pikapuuu.17.6@gmail.com>
2022-03-28 23:19:53 +0900

    fix_app.js-error add_navbar.jsx-lists

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
+
+import images from '../../constants/images'
 
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app_navbar'>
+
+    <div className='app_navbar-logo'>
+      <img src={images.gericht} alt="app logo" />
+    </div>
+    <ul className='app_navbar-links'>
+      <li className='p__opensans'><a href="#home">home</a></li>
+      <li className='p__opensans'><a href="#about">about</a></li>
+      <li className='p__opensans'><a href="#menu">menu</a></li>
+      <li className='p__opensans'><a href="#awards">awards</a></li>
+      <li className='p__opensans'><a href="#contact">contact</a></li>
+    </ul>
+  </nav>
 );
+
 
 export default Navbar;
